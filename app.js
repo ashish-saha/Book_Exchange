2b06d2e51e522f6f70a13fa58d2cafdc323a5926
Raul Alvarez <raulito2394@gmail.com>
2016-09-23 11:17:45 -0400

    Have handlebars working

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
+// Setting up middleware
 var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
 var handlebars = require('express-handlebars');
 
+// Express for ease
 var app = express();
-var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
+// Public directory for scripts, css, images, etc
 app.use('/static', express.static(__dirname + '/public'));
-// app.set('views', path.join(__dirname, 'views'));
 
+// Handlebars setup
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
 
+// Relative to mongoose and MongoDB
+var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 mongoose.connect('mongodb://localhost/Book_Exchange', function(err,res){
 	if(!err){
 		console.log("Connected to database!");
@@ -21,12 +26,15 @@ mongoose.connect('mongodb://localhost/Book_Exchange', function(err,res){
 	}
 });
 
+// First route but should be moved to its own directory
 app.get('/', function(req, res){
-	res.send('Hello World');
+	res.render('home');
 });
 
+// Export
 module.exports = app;
 
+// Listening to port 8000, localhost:8000
 app.listen(8000, function(){
 	console.log('WORKING ON 8000');
 });
